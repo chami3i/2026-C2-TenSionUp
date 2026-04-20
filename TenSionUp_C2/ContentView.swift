218dee7999141dd5619b1940f91f6e6de0615d18
chami3i <hongcm2002@naver.com>
2026-04-20 20:48:02 +0900

    Initial Commit

---
+++ TenSionUp_C2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TenSionUp_C2
+//
+//  Created by chaem on 4/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TenSionUp_C2/TenSionUp_C2App.swift
@@ -0,0 +1,17 @@
+//
+//  TenSionUp_C2App.swift
+//  TenSionUp_C2
+//
+//  Created by chaem on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct TenSionUp_C2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
