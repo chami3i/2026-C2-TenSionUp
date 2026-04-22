2068e68ca35dbf6a9179e7ae6f53bc7a64acf1e8
chami3i <hongcm2002@naver.com>
2026-04-22 17:57:10 +0900

    refactor: update Speaker model for identity and timing tracking
    - Speaker를 Codable로 변경하여 저장 가능하도록 수정
    - UUID 기반 id 추가로 참가자 고유 식별
    - speakingSeconds, listeningSeconds 추가로 발화/청취 시간 기록
    - 기본값 설정을 위한 init 추가

--- TenSionUp_C2/Speaker.swift
+++ TenSionUp_C2/Speaker.swift
@@ -7,10 +7,34 @@
 
 import Foundation
 
-struct Speaker: Codable, Hashable {
+enum TimeState: String, Codable {
+    case notStarted
+    case running
+    case paused
+    case ended
+}
+
+struct Speaker: Codable, Identifiable {
+    let id : UUID
     var name: String
     var image: String
-    var time: Int
-//    var speakingSeconds: Int
-//    var listeningSeconds: Int
+    var time: Int                               // 1인당 발화 시간
+    var timeState: TimeState = .notStarted
+    var speakingSeconds: Int                    // 말하는 시간
+    var listeningSeconds: Int                   // 들은 시간 time - speakingSeconds
+    
+    init(
+        id: UUID = UUID(),
+        name: String, image:
+        String, time: Int,
+        speakingSeconds: Int = 0,
+        listeningSeconds: Int = 0
+    ) {
+        self.id = id
+        self.name = name
+        self.image = image
+        self.time = time
+        self.speakingSeconds = speakingSeconds
+        self.listeningSeconds = listeningSeconds
+    }
 }
